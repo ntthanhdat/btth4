3b93a0cc8d7510a787196419acc708368c28800f
ntthanhdat <thanhdatnguyenthe@gmail.com>
2020-12-31 00:59:36 +0700

    than gui hateo

--- admin/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn=mysqli_connect('localhost', 'root', '', 'blogth4');
-if(!$conn){
-    die("connect fail ".mysqli_connect_error());
-}
-?>
--- admin/error.php
+++
@@ -1,4 +0,0 @@
-<?php
-$error=$_GET['error'];
-echo "Error:  $error";
-?>
